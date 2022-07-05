e4c29bbb7a65e149d6d8767b17732d6a025ecdf0
Rameshwar Mangnale <mangnalercm97@gmail.com>
2022-07-05 15:41:12 +0530

    fixed issue responsive

--- packages/comps/Cards.jsx
+++ packages/comps/Cards.jsx
@@ -4,14 +4,14 @@ export default function Cards() {
   return (
     <>
       <div className="text-3xl bg-orange">main</div>
-      <div className="max-w-5xl m-auto mx-5 min-w-5xl">
+      <div className="max-w-5xl min-w-full mx-3">
         <div className="grid grid-cols-3 gap-2">
           <div>
             <Image
               src="/Images/burger.png"
               alt="product Image"
-              width={100}
-              height={120}
+              width={96}
+              height={96}
             />
             <p className="text-base ">
               Burger . <span className="font-bold">$4.99</span>{" "}
@@ -22,68 +22,68 @@ export default function Cards() {
             <Image
               src="/Images/fries.png"
               alt="product Image"
-              width={100}
-              height={120}
+              width={96}
+              height={96}
             />
             <p className="text-base ">
               Fries . <span className="font-bold" >$4.99</span>{" "}
             </p>
-            <button className="px-8 py-2 text-xl font-bold text-white rounded-xl bg-orange">ADD</button>
+            <button className="px-8 py-2 text-lg font-bold text-white rounded-xl bg-orange">ADD</button>
           </div>
           <div>
             <Image
               src="/Images/hot_dog.png"
               alt="product Image"
-              width={100}
-              height={120}
+              width={96}
+              height={96}
             />
             <p className="text-base ">
               Hotdog . <span className="font-bold">$4.99</span>{" "}
             </p>
-            <button className="px-8 py-2 text-xl font-bold text-white rounded-xl bg-orange">ADD</button>
+            <button className="px-8 py-2 text-lg font-bold text-white rounded-xl bg-orange">ADD</button>
           </div>
           <div>
             <Image
               src="/Images/Kebab.png"
               alt="product Image"
-              width={100}
-              height={120}
+              width={96}
+              height={96}
             />
-            <p className="text-lg ">
+            <p className="text-base ">
               Kebab . <span className="font-bold">$4.99</span>{" "}
             </p>
-            <button className="px-8 py-2 text-xl font-bold text-white rounded-xl bg-orange">ADD</button>
+            <button className="px-8 py-2 text-lg font-bold text-white rounded-xl bg-orange">ADD</button>
           </div>
           <div>
             <Image
               src="/Images/pizza.png"
               alt="product Image"
-              width={100}
-              height={120}
+              width={96}
+              height={96}
             />
-            <p className="text-lg ">
+            <p className="text-base ">
               Pizza . <span className="font-bold">$4.99</span>{" "}
             </p>
-            <button className="px-8 py-2 text-xl font-bold text-white rounded-xl bg-orange">ADD</button>
+            <button className="px-8 py-2 text-lg font-bold text-white rounded-xl bg-orange">ADD</button>
           </div>
           <div>
             <Image
               src="/Images/donuts.png"
               alt="product Image"
-              width={100}
-              height={120}
+              width={96}
+              height={96}
             />
-            <p className="text-lg ">
+            <p className="text-base ">
               Donuts . <span className="font-bold">$4.99</span>{" "}
             </p>
-            <button className="px-8 py-2 text-xl font-bold text-white rounded-xl bg-orange">ADD</button>
+            <button className="px-8 py-2 text-lg font-bold text-white rounded-xl bg-orange">ADD</button>
           </div>
           <div>
             <Image
               src="/Images/popcorn.png"
               alt="product Image"
-              width={100}
-              height={120}
+              width={96}
+              height={96}
             />
             <p className="text-base ">
               Popcorn . <span className="font-bold">$4.99</span>{" "}
@@ -94,8 +94,8 @@ export default function Cards() {
             <Image
               src="/Images/coca.png"
               alt="product Image"
-              width={100}
-              height={120}
+              width={96}
+              height={96}
             />
             <p className="text-base ">
               Coke . <span className="font-bold">$4.99</span>{" "}
@@ -106,8 +106,8 @@ export default function Cards() {
             <Image
               src="/Images/icecream.png"
               alt="product Image"
-              width={100}
-              height={120}
+              width={96}
+              height={96}
             />
             <p className="text-base ">
               Icecream . <span className="font-bold">$4.99</span>{" "}
