69f2566bb48caab2c4028233cb42cc87a898e868
Rameshwar Mangnale <mangnalercm97@gmail.com>
2022-07-05 12:01:55 +0530

    add changes

--- packages/comps/Cards.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Cards() {
-  return (
-    <div className='text-3xl  bg-orange'>main</div>
-  )
-}
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
 
 import Head from 'next/head'
 import Image from 'next/image'
-import Cards from '../packages/comps/Cards'
 import React from 'react'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <>
-<Cards></Cards> 
+<p>food</p>
  </>
   )
 }
