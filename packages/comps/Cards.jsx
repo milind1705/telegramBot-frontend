9bf889ae10dd443f8e647c26c4218541f74074ba
Rameshwar Mangnale <mangnalercm97@gmail.com>
2022-07-07 13:12:22 +0530

    add button view add

--- packages/comps/Cards.jsx
+++ packages/comps/Cards.jsx
@@ -76,7 +76,7 @@ export default function Cards() {
 
   return (
     <>
-      <div className="text-3xl bg-orange">main</div>
+      {/* <div className="text-3xl bg-orange">main</div> */}
       <div className="max-w-5xl m-auto mx-5 min-w-5xl">
         <div className="grid grid-cols-3 gap-2">
           {items.map((item) =>{
@@ -101,6 +101,9 @@ export default function Cards() {
             )
           })}
           
+        </div>
+        <div className="py-2 pt-8">
+        <button type="button" className=" w-full max-w-3xl focus:outline-none text-white bg-green  focus:ring-4  font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">VIEW ORDER</button>
         </div>
       </div>{" "}
     </>
