97fe8ca1482ddf7e5f528162bffd42a43425426c
Rameshwar Mangnale <mangnalercm97@gmail.com>
2022-07-07 17:58:21 +0530

    button issue fixed

--- packages/comps/Button.js
+++ packages/comps/Button.js
@@ -1,27 +1,25 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { MinusIcon, PlusIcon } from '@heroicons/react/solid'
-import {useState} from "react"
+import { MinusIcon, PlusIcon } from "@heroicons/react/solid";
+import { useState } from "react";
 
-export default function Button({noOfItems, setNoOfItems}) {
-  const decreamentItem = () =>{
-    if(noOfItems > 0 ){
-      setNoOfItems(noOfItems - 1)
+export default function Button({ noOfItems, setNoOfItems }) {
+  const decreamentItem = () => {
+    if (noOfItems > 0) {
+      setNoOfItems(noOfItems - 1);
+    } else {
+      setNoOfItems(0);
     }
-    else{
-      setNoOfItems(0)
-    }
-    
-  }
-  const increamentItem = () =>{
-    setNoOfItems(noOfItems + 1)
-  }
+  };
+  const increamentItem = () => {
+    setNoOfItems(noOfItems + 1);
+  };
   return (
     <span className="relative z-0 inline-flex gap-2 shadow-sm">
       <button
         type="button"
         className="relative inline-flex items-center px-2.5 py-1.5 rounded-md border border-gray-300 bg-red text-sm font-medium text-white hover:bg-gray-50 "
-          onClick={decreamentItem}
-     >
+        onClick={decreamentItem}
+      >
         <span className="sr-only">Previous</span>
         <MinusIcon className="w-5 h-5" aria-hidden="true" />
       </button>
@@ -29,10 +27,10 @@ export default function Button({noOfItems, setNoOfItems}) {
         type="button"
         className="-ml-px relative inline-flex items-center px-2.5 py-1.5 rounded-md border border-gray-300 bg-orange text-sm font-medium text-white hover:bg-gray-50 "
         onClick={increamentItem}
-        >
+      >
         <span className="sr-only">Next</span>
         <PlusIcon className="w-5 h-5" aria-hidden="true" />
       </button>
     </span>
-  )
+  );
 }
--- packages/comps/Card.jsx
+++ packages/comps/Card.jsx
@@ -1,48 +1,52 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Button from "./Button";
 
-export default function Cards({food}) {
-    const [showAddButton, setShowAddButton]= useState(true)
-  const [noOfItems, setNoOfItems] =useState(0)
-  const { name, price, image} = food
+export default function Cards({ food }) {
+  const [showAddButton, setShowAddButton] = useState(true);
+  const [noOfItems, setNoOfItems] = useState(0);
+  const { name, price, image } = food;
 
-  useEffect(()=>{
-    if(noOfItems !== 0 ){
-      setShowAddButton(false)
+  useEffect(() => {
+    if (noOfItems !== 0) {
+      setShowAddButton(false);
     }
-    if(noOfItems === 0 ){
-      setShowAddButton(true)
+    if (noOfItems === 0) {
+      setShowAddButton(true);
     }
-  }, [noOfItems])
+  }, [noOfItems]);
   return (
     <>
       <div className="max-w-5xl m-auto mx-5 min-w-5xl">
         <div className="grid grid-cols-3 gap-2">
-         
-              <div  className="w-32 px-2 ">
-            {showAddButton ? null : <div 
-           className="w-6 h-6 mt-2 ml-20 -mb-4 text-center text-white rounded-full bg-orange">{noOfItems}</div>}
-          
-            <Image
-              src={image}
-              alt="product Image"
-              width={80}
-              height={80}
-            />
+          <div className="w-32 px-2 ">
+            {showAddButton ? null : (
+              <div className="w-6 h-6 ml-20 -mb-6 text-center text-white rounded-full bg-orange">
+                {noOfItems}
+              </div>
+            )}
+
+            <Image src={image} alt="product Image" width={80} height={80} />
             <p className="mb-1 text-base ">
-              {name}.<span className="font-bold" >₹{noOfItems === 0 ? price : noOfItems * price}</span>{" "}
+              {name}.
+              <span className="font-bold">
+                ₹{noOfItems === 0 ? price : noOfItems * price}
+              </span>{" "}
             </p>
             <div>
-              {showAddButton ? <button className="px-7 py-1.5 text-lg font-base text-white rounded-xl bg-orange" 
-             onClick={()=>setNoOfItems(1)}
-             >ADD</button> : <Button noOfItems ={noOfItems} setNoOfItems = {setNoOfItems}/>  } 
+              {showAddButton ? (
+                <button
+                  className="px-7 py-1.5 text-lg font-base text-white rounded-xl bg-orange"
+                  onClick={() => setNoOfItems(1)}
+                >
+                  ADD
+                </button>
+              ) : (
+                <Button noOfItems={noOfItems} setNoOfItems={setNoOfItems} />
+              )}
             </div>
           </div>
-            
-          
         </div>
-       
       </div>{" "}
     </>
   );
