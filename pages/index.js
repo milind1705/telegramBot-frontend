486389c04da1035ea37fbe4483f241f6d7f0b9f9
Rameshwar Mangnale <mangnalercm97@gmail.com>
2022-07-08 19:10:00 +0530

    fixed issue view order button

--- pages/index.js
+++ pages/index.js
@@ -39,7 +39,7 @@ export default function Home() {
   return (
     <>
       <div
-        className={`max-w-5xl m-auto mx-5 min-w-5xl ${
+        className={`max-w-5xl m-auto  min-w-5xl ${
           showCheckout ? "hidden" : ""
         }`}
       >
@@ -57,7 +57,7 @@ export default function Home() {
             );
           })}
         </div>
-        <div className="py-2 pt-8">
+        <div className="py-2 pt-8 pl-4">
           <button
             type="button"
             className=" w-full max-w-3xl focus:outline-none text-white bg-green  focus:ring-4  font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
